8de9ede0bde74965bf0a2f784faef4b74fb1f04a
Serhii <sergrika16@gmail.com>
2024-09-22 22:05:24 +0300

    new file:   gIgnrore.gitignore 	new file:   material/logo.png 	new file:   site/css header.css 	new file:   site/index.php 	new file:   site/index.php.html 	new file:   site/test.html

---
+++ site/index.php
@@ -0,0 +1,11 @@
+<?php
+$currentUrl = $_SERVER['REQUEST_URI'];
+
+// Массив ссылок меню
+$menuItems = [
+    ['url' => '/', 'title' => 'Главная'],
+    ['url' => '/about', 'title' => 'О нас'],
+    ['url' => '/contact', 'title' => 'Контакты']
+
+];
+echo "hello world";
